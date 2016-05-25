952bab66dfa383bdffbd2f86ab8d6c074a00c06f
avengermsoft <avenger_msoft@mail.ru>
2016-05-25 11:58:46 +0300

    Delete _Version.bat

--- udr_SphinxClient/_Version.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SET BRCC="C:\PROGRA~2\EMBARC~1\RADSTU~1\8.0\bin\brcc32.exe"
-SET INCVERRCFILE="D:\Install\Project\New\AutoNewFileVerInfo\IncVerRcFile\IncVerRcFile.exe"
-Set PROJECTNAME=udr_SphinxClient
-
-del %PROJECTNAME%.res >nul 2>nul
-%INCVERRCFILE% -f%PROJECTNAME%.rc 
-%BRCC% %PROJECTNAME%.rc
