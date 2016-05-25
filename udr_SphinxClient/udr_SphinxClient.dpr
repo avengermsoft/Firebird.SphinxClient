aeef3a79da5bcb22caee94b8393f2f2937ed2564
avengermsoft <avenger_msoft@mail.ru>
2016-05-25 11:54:38 +0300

    Add files via upload

---
+++ udr_SphinxClient/udr_SphinxClient.dpr
@@ -0,0 +1,20 @@
+library udr_SphinxClient;
+
+{$WEAKLINKRTTI ON}
+{$RTTI EXPLICIT METHODS([]) PROPERTIES([]) FIELDS([])}
+
+{$R udr_SphinxClient.res}
+
+uses
+  Firebird in 'Includes\Firebird.pas',
+  UdrInit in 'Includes\UdrInit.pas',
+  u_SphinxMySQL in '..\u_SphinxMySQL.pas',
+  UdrSphinxClientExecute in 'Includes\UdrSphinxClientExecute.pas',
+  UdrQuotedStr in 'Includes\UdrQuotedStr.pas';
+
+exports firebird_udr_plugin;
+
+//begin
+  //IsMultiThread := True;
+
+end.
