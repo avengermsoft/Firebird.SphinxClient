ea2e196e8f0a24a2a0ea4ec5243c8075669f34b4
avengermsoft <avenger_msoft@mail.ru>
2016-06-02 11:49:39 +0300

    Add files via upload

--- udf_SphinxClient/udf_SphinxClient.dpr
+++ udf_SphinxClient/udf_SphinxClient.dpr
@@ -1,9 +1,14 @@
 library udf_SphinxClient;
 
-{$WEAKLINKRTTI ON}
-{$RTTI EXPLICIT METHODS([]) PROPERTIES([]) FIELDS([])}
+{$IFDEF FPC}
+  {$MODE objfpc}
+  {$H+}
+{$ELSE}
+  {$WEAKLINKRTTI ON}
+  {$RTTI EXPLICIT METHODS([]) PROPERTIES([]) FIELDS([])}
 
-{$R udf_SphinxClient.res}
+  {$R udf_SphinxClient.res}
+{$ENDIF}
 
 uses
   u_SphinxMySQL in '..\u_SphinxMySQL.pas',
